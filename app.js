7cbe4cddc67d9bd388917bc139ab7bf9cd1e6d51
Ellezique <gizellevanzyl@gmail.com>
2021-07-15 12:18:52 +1000

    Week 4 Thursday code along before deployment.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const cardsRouter = require('./routes/cardsRoutes') 
+const authRouter = require('./routes/authRoutes') 
 const port = 4000
 const app = express()
 const dbConn = 'mongodb://localhost/trello_clone_db'
@@ -13,7 +15,8 @@ mongoose.connect(dbConn,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useFindAndModify: false
+        useFindAndModify: false,
+        useCreateIndex: true
     },
     err => {
         if (err){
@@ -23,11 +26,30 @@ mongoose.connect(dbConn,
         }
     }
 )
+
+app.use((req, res, next) => {
+    console.log("headers: ",req.headers.authorization)
+    if(req.headers && req.headers.authorization){
+        jwt.verify(req.headers.authorization.split(' ')[1], "backend-best-end",(err, decode)=>{
+            if (err) {
+                req.user = undefined
+            }else{
+                req.user = decode
+            }
+            next()
+        })
+    }else{
+        req.user = undefined
+        next()
+    }
+})
+
 app.get("/", (req, res) => {
     res.send("Hello world!")
     })
 
 app.use("/cards", cardsRouter)
+app.use("/auth", authRouter)
 app.listen(port, ()=>{console.log(`Trello clone server is running on port ${port}`)})
 
 
